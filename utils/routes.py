be6ae4c94b9b644fbc583480b1920194c76dd188
YaronP <yaroon15@gmail.com>
2022-03-19 22:14:44 +0200

    fixed routes.py

--- utils/routes.py
+++ utils/routes.py
@@ -3,9 +3,11 @@ dbDir = 'utils\\DataBase\\'
 usersFile = f'{dbDir}users.json'
 rulesFile = f'{dbDir}rules.json'
 latestCsvFile = f'{dbDir}uploaded.json'
+
 # path to icons
-sceLogo = 'utils\\Images\\SCElogo.png'
-importCsvLogo = 'utils\\Images\\csv1.png'
+imgDir = 'utils\\Images\\'
+sceLogo = f'{imgDir}SCElogo.png'
+importCsvLogo = f'{imgDir}csv1.png'
 
 # path to Cyco CSVs
 issues_path = 'CyCog\\Cyco_Assets.csv'
