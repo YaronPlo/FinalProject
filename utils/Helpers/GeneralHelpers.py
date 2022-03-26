4bcf103e27f09bb4925249b3ce21487fa659ed12
YaronP <yaroon15@gmail.com>
2022-03-26 23:36:00 +0300

    show table in AnalystDashboard.py

--- utils/Helpers/GeneralHelpers.py
+++ utils/Helpers/GeneralHelpers.py
@@ -11,7 +11,7 @@ def fillTableData(df, table):
     table.setRowCount(len(df))
     table.setColumnCount(len(df.columns))
 
-    # Fill the Headers in the Table
+    # Fill the Headers rows and cols in the Table
     table.setHorizontalHeaderLabels(colName for colName in df.columns)
     table.setVerticalHeaderLabels(str(rowName) for rowName in df.index)
     for rows in range(len(df)):
