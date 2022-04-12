44f84b3a5e774e912b51fcb3fad6d5b29b30fde7
chenaz1 <57365377+chenaz1@users.noreply.github.com>
2022-04-12 03:59:41 +0300

    Basic graphs to GeneralHelpers

--- utils/Helpers/GeneralHelpers.py
+++ utils/Helpers/GeneralHelpers.py
@@ -5,8 +5,11 @@ import string
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.corpus import stopwords
-import Data.Utilities as Data
 import pandas as pd
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
 
 __all__ = ["fillTableData", "currentLoggedInUpdate", "find_most_influential", "cosine_sim_vectors", "clean_string"]
 
@@ -61,21 +64,11 @@ def find_most_influential(df, raw_df):
     round = 0
     result = {}
     df = adding_similarity_column(df, raw_df)
-    # df = pd.merge(df, raw_df[['Title', 'Remediation Steps']], left_index=True,
-    #               right_index=True)  # adding two columns to df from origin df
-    # df[similarity] = df['Remediation Steps'] + ' ' + df['Title']  # unite two added columns to one
-    # df.loc[:, similarity] = df[similarity].apply(lambda x: clean_string(x))
     for index1, row1 in df.iterrows():  # iterate over df rows
         round += 1
         if index1 in [x for xs in result.values() for x in xs]:  # if this issue already treated skip it
             continue
         result[index1] = affected_issues(df[round:], result.values(), row1[similarity])
-        # for index2, row2 in df[round:].iterrows():  # iterate over df rows from index1 + 1
-        #     if index2 in [x for xs in result.values() for x in xs]:
-        #         continue
-        #     cos_sim = cosine_sim_vectors(row1[similarity], row2[similarity])  # calculate similarity
-        #     if cos_sim > 0.7:
-        #         result[index1] += [index2]
     return result
 
 
@@ -99,5 +92,38 @@ def adding_similarity_column(df, raw_df):
     df.loc[:, similarity] = df[similarity].apply(lambda x: clean_string(x))
     return df
 
+
 # example of using adding_similarity_column for single issue:
 # arr = affected_issues(cleaned_df,[],cleaned_df.loc[2]['similarity'])
+
+
+def graph_1(analysts_ID, analysts_daily_avg):  # Daily ability
+    fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
+    ax.bar(analysts_ID, analysts_daily_avg)
+    ax.set_xlabel('Analyst ID')
+    ax.set_ylabel('Daily avg')
+    ax.set_title('Daily ability')
+    plt.show()
+
+
+def graph_2(analysts_ID, duration_mean):  # issues duration-mean (in-prog -> done) Comparison
+    fig, ax = plt.subplots()
+    ax.plot(analysts_ID, duration_mean)
+    ax.set_xlabel('Analyst ID')
+    ax.set_ylabel('Duration mean [h]')
+    ax.set_title('duration-mean Comparison ')
+    plt.show()
+
+
+def graph_3(analyst_ID, issues_duration, issues_impact):  # impact against time to complete per analyst
+    fig, ax = plt.subplots(figsize=(5, 2.7))
+    ax.scatter(issues_duration, issues_impact, s=50, facecolor='C0', edgecolor='k')
+    ax.set_xlabel('Duration')
+    ax.set_ylabel('Impact')
+    ax.set_title("{} impact VS duration.".format(analyst_ID))
+    plt.show()
+
+
+# graph_1(['analyst_1', 'analyst_2', 'analyst_3', 'analyst_4'], [3, 1, 5, 7])
+# graph_2(['analyst_1', 'analyst_2', 'analyst_3', 'analyst_4'], [2.5, 1, 3.5, 3])
+# graph_3('analyst_1', [3, 2, 1.5, 5, 3.2, 4], [20, 45, 15, 50, 30, 40])
