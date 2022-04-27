57eee098f6d43ae766387e8f8c72e0f89f82b6a4
chenaz1 <57365377+chenaz1@users.noreply.github.com>
2022-04-27 14:31:04 +0300

    StatHelper

--- utils/Helpers/StatisticsHelper.py
+++ utils/Helpers/StatisticsHelper.py
@@ -1,8 +1,14 @@
 import matplotlib.pyplot as plt
 from utils.routes import *
 from multiprocessing import Process
+import utils.routes
+import pandas as pd
+from matplotlib.backends.backend_pdf import PdfPages
+from Data.Utilities import open_csv
+from utils.Helpers.AnalystHelper import daily_avg_issues, done_issue_avg
 
-__all__ = ["call_stat_graph", "graph_1", "graph_2", "graph_3"]
+__all__ = ["call_stat_graph", "graph_1", "graph_2", "graph_3","daily_avg_issues","done_issue_avg"]
+
 
 
 # This function generates the graph using multiproccess
@@ -47,3 +53,31 @@ def graph_3(analyst_ID, issues_duration, issues_impact):  # impact against time 
 # graph_1(['analyst_1', 'analyst_2', 'analyst_3', 'analyst_4'], [3, 1, 5, 7])
 # graph_2(['analyst_1', 'analyst_2', 'analyst_3', 'analyst_4'], [2.5, 1, 3.5, 3])
 # graph_3('analyst_1', [3, 2, 1.5, 5, 3.2, 4], [20, 45, 15, 50, 30, 40])
+
+def get_graphs_pdf():
+    df = open_csv(utils.routes.status_table)
+    analyst_list = list(set(df['Analyst Handler']))
+    daily_avg_list = []
+    done_avg_list = []
+
+    for analyst in analyst_list:
+        analyst_daily_avg = daily_avg_issues(df, analyst)
+        daily_avg_list.append(analyst_daily_avg['ave_per_day'])
+
+        analyst_done_avg = done_issue_avg(df, analyst)
+        done_avg_list.append(round(analyst_done_avg['duration_mean']))
+
+    analyst_list.append('itay')  # for example
+    daily_avg_list.append(4)  # for example
+    done_avg_list.append(15)  # for example
+
+    pdf = PdfPages('analystStat.pdf')
+
+    graph_2(analyst_list, done_avg_list)
+    pdf.savefig()
+    graph_1(analyst_list, daily_avg_list)
+    pdf.savefig()
+    pdf.close()
+
+
+# get_graphs_pdf()
