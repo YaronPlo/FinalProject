3f007ba3e141c4cb07253a816d386e1f5e74b199
YaronP <yaroon15@gmail.com>
2022-04-29 18:50:35 +0300

    small updates

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
 from PyQt5 import QtWidgets
 from Components import Login
-
+from utils.Helpers.GeneralHelpers import default_rules
 
 
 def Main():
+    default_rules()
+
     app = QtWidgets.QApplication(sys.argv)
     Login.RunLogIn()
     sys.exit(app.exec_())
