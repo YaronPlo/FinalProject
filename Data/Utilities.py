cbee22cd33db98e5b5200ee5334e3137d6f5956b
chenaz1 <57365377+chenaz1@users.noreply.github.com>
2022-05-10 17:02:09 +0300

    delete unnecessary files graph 4 added

--- Data/Utilities.py
+++ Data/Utilities.py
@@ -2,6 +2,10 @@ import pandas as pd
 from utils import routes
 from datetime import datetime
 
+__all__ = ["open_csv","cat_to_num","num_to_bins","sorting_df","show_only","dont_show","str_to_datatime","get_now",
+           "return_N_oldest","letters_to_numbers","potential_Impact_column","key_word","unique_to_numbers","weights_calc",
+           "WSM","table_preprocess"]
+
 
 def open_csv(path):
     try:
@@ -9,24 +13,7 @@ def open_csv(path):
     except:
         return pd.read_csv(f'..\\{path}', low_memory=False)
 
-
-def table_description(_df):
-    print('number of column:', _df.shape[1])
-    print('number of columns which is NaN:',
-          len(_df[_df.columns[_df.isnull().all()]].columns))  # ),'\n',df[df.columns[df.isnull().all()]].columns)
-    print()
-
-
-# Severity, Asset Security Grade, Asset Security Score, Asset Discoverability, Asset Attractiveness
-# numeric columns: Asset Security Score(continuous),
-# Alpha-betic columns:Severity(4),Asset Security Grade (16), Asset Discoverability(4), Asset Attractiveness(4)
-
 pd.reset_option("max_columns")
-
-# cwd = os.getcwd()  # Get the current working directory (cwd)
-# files = os.listdir(cwd)  # Get all the files in that directory
-# print("Files in %r: %s" % (cwd, files))
-
 raw_dataFrame = open_csv(routes.issues_path)
 
 relevant_columns = {1: 'Severity',
@@ -55,15 +42,6 @@ def cat_to_num(df, col_list, catagories):
         df.loc[:, _] = df[_].map(catagories)
     return df
 
-
-def show_table_head(df):
-    print(df.head().to_string())
-
-
-def show_table(df1):
-    print(df1.to_string())
-
-
 def num_to_bins(df, col_list, num_of_bins):
     for _ in col_list:
         temp = pd.DataFrame(df[_])
@@ -72,15 +50,12 @@ def num_to_bins(df, col_list, num_of_bins):
         # print(temp)
     return df
 
-
 def sorting_df(df, col=['Asset Security Grade', 'Asset Security Score']):
     df_sorted = df.sort_values(by=col, inplace=False, ascending=[False, False])
     return df_sorted
 
-
 def show_only(df, column_name, values):  # only_values:list
     # filtered_df = df.loc[df[column_name].isin(values)]
-    # values = '&'.join(values)  # '&' for and statemennt
     for val in values:
         mask = df[column_name].apply(lambda sent: val in sent)
         filtered_df = df[mask]
@@ -93,53 +68,39 @@ def show_only(df, column_name, values):  # only_values:list
     print('filtered_df')
     return filtered_df
 
-
 def dont_show(df, column_name, values):  # only_values:list
     df = df.loc[~df[column_name].isin(values)]
     return df
-
 
 def str_to_datatime(df, col_list):
     for _ in col_list:
         df.loc[:, _] = df[_].apply(lambda x: datetime.strptime(x, '%Y-%m-%dT%H:%M:%S.%fZ'))
     # return df
 
-
 def get_now():
     now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")  # time object
     return now
-
-
-# def oldest(datatime_list):
-#     df = df1.copy()
-#     df = df.loc[df['Asset First Seen'] == min(datatime_list)]
-#     return df
 
 def return_N_oldest(df, n):
     df = df.sort_values(by=['Asset First Seen'])
     df.reset_index(drop=True, inplace=True)
     return df.head(n)
 
-
 def letters_to_numbers(df, columns):
     for col in columns:
         df.loc[:, col] = [ord(x) - 64 if type(x) == str else x for x in df[col]]
     return df
 
-
-def Potential_Impact_column(df):  # clean string
+def potential_Impact_column(df):  # clean string
     banned = ['Loss', 'of', '|']
     df['Potential Impact'] = df['Potential Impact'].apply(
         lambda sent: (" ".join(x.lower() for x in sent.replace('|', '').split(' ') if x not in banned)).split(' '))
     # df = key_word(df, 'Potential Impact', key_word)
-    # print('->',df['Potential Impact'])
     return df
-
 
 def key_word(df, col='Description', word='HTTP'):
     df = df.loc[lambda sent: sent[col].apply(lambda l: word.lower() in l)]
     return df
-
 
 def unique_to_numbers(df, col):
     unique = dict(enumerate(df[col].unique()))
@@ -147,17 +108,15 @@ def unique_to_numbers(df, col):
     df[col].replace(unique, inplace=True)
     return df
 
-
 def weights_calc(weights):
     list = []
     summ = sum(weights)
     for elem in weights:
         try:
-            list.append(round(elem/summ,3))
+            list.append(round(elem / summ, 3))
         except ZeroDivisionError:
             list.append(0)
     return list
-
 
 def WSM(df, weights):  # Weighted Sum Method – Multi Criteria Decision-Making
     col = ['Severity', 'Asset Security Grade', 'Asset Security Score', 'Asset Discoverability']
@@ -182,15 +141,13 @@ def WSM(df, weights):  # Weighted Sum Method – Multi Criteria Decision-Making
     # print(df.head(10).to_string())
     return df
 
-
 def table_preprocess(df, relevant_col, catagories_list):
     # print(df.shape)
-    df = Potential_Impact_column(df)
+    df = potential_Impact_column(df)
     df['Description'] = df['Description'].apply(lambda sent: [x.lower() for x in sent.split(' ') if x.isalpha()])
     df = df[relevant_col.values()]
     df = cat_to_num(df, ['Severity', 'Asset Discoverability', 'Asset Attractiveness'], catagories_list)
     str_to_datatime(df, ['Asset First Seen'])
     return df
-
 
 dataFrame = table_preprocess(raw_dataFrame, relevant_columns, catagories)
